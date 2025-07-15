49d534102d1f43b8c3c4c48634ec45b1341ec98a
Foster13 <muhmdfaza@gmail.com>
2025-07-15 15:08:16 +0700

    feat:add InsightCermat

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Jumbotron from "@/components/Jumbotron";
+import InsightCermat from "@/components/InsightCermat";
 import IntroSection from "@/components/KenalanSama";
 import InvestSection from "@/components/MulaiInvestasi";
 
 export default function Home() {
   return (
     <main>
+      <InsightCermat />
       <Jumbotron />
       <IntroSection />
       <InvestSection />
